867cf7b0f2a22a6aea67ee8598810cdbc960b058
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2020-08-05 06:07:41 +0530

    checkpoint

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -15,102 +15,100 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     ScreenUtil.init(context);
     return Scaffold(      
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: Center(
-              child: Column(              
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(height: 85,),
-                  Container(            
-                    width: 170,
-                    height: 207,
-                    child: Image.asset("assets/Logo.png"),
-                  ),
-                  SizedBox(height: 45,),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 40),
-                    child: Form(
-                      key: formKey,
-                      child: Column(
-                        children: [
-                          // Email address
-                          Container(
-                            padding: EdgeInsets.symmetric(horizontal: 20),
-                            width: 296,
-                            height: 52,                      
-                            decoration: BoxDecoration(                        
-                              border: Border.all(width: 1, color: Colors.black),
-                              borderRadius: BorderRadius.circular(30)
-                            ),                  
-                            child: TextFormField(
-                              // validator: (email) {
-                              //       return RegExp(
-                              //        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+"
-                              //      ).hasMatch(email) ?
-                              //        null :
-                              //        "Please provide a valid email id";
-                              //     },
-                              decoration: InputDecoration(
-                                hintText: "Email",
-                                hintStyle: TextStyle(color: Colors.black, fontFamily: "Quicksand"),
-                                border: InputBorder.none
-                              ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Center(
+            child: Column(              
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 85,),
+                Container(            
+                  width: 170,
+                  height: 207,
+                  child: Image.asset("assets/images/Logo.png"),
+                ),
+                SizedBox(height: 45,),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        // Email address
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          width: 296,
+                          height: 52,                      
+                          decoration: BoxDecoration(                        
+                            border: Border.all(width: 1, color: Colors.black),
+                            borderRadius: BorderRadius.circular(30)
+                          ),                  
+                          child: TextFormField(
+                            // validator: (email) {
+                            //       return RegExp(
+                            //        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+"
+                            //      ).hasMatch(email) ?
+                            //        null :
+                            //        "Please provide a valid email id";
+                            //     },
+                            decoration: InputDecoration(
+                              hintText: "Email",
+                              hintStyle: TextStyle(color: Colors.black, fontFamily: "Quicksand"),
+                              border: InputBorder.none
                             ),
                           ),
-                          SizedBox(height: 10,),
-                          // Password
-                          Container(
-                            padding: EdgeInsets.symmetric(horizontal: 20),
-                            width: 296,
-                            height: 52,                      
-                            decoration: BoxDecoration(
-                              // color: Colors.redAccent,
-                              border: Border.all(width: 1, color: Colors.black),
-                              borderRadius: BorderRadius.circular(30)
-                            ),                  
-                            child: TextFormField(
-                              // validator: (password){
-                              //   return password.length > 6 ?
-                              //     null : 
-                              //     "Please provide a password which has more than 6 characters";
-                              // },
-                              decoration: InputDecoration(
-                                hintText: "Password",
-                                hintStyle: TextStyle(color: Colors.black, fontFamily: "Quicksand"),
-                                border: InputBorder.none
-                              ),
+                        ),
+                        SizedBox(height: 10,),
+                        // Password
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          width: 296,
+                          height: 52,                      
+                          decoration: BoxDecoration(
+                            // color: Colors.redAccent,
+                            border: Border.all(width: 1, color: Colors.black),
+                            borderRadius: BorderRadius.circular(30)
+                          ),                  
+                          child: TextFormField(
+                            // validator: (password){
+                            //   return password.length > 6 ?
+                            //     null : 
+                            //     "Please provide a password which has more than 6 characters";
+                            // },
+                            decoration: InputDecoration(
+                              hintText: "Password",
+                              hintStyle: TextStyle(color: Colors.black, fontFamily: "Quicksand"),
+                              border: InputBorder.none
                             ),
-                          )
-                        ],
-                      ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                  SizedBox(height: 10,),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 40),
-                    child: Container(
-                        alignment: Alignment.center,
-                        width: 296,
-                        height: 52,                     
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [Hexcolor("#EF5A53"), Hexcolor("#F48348"), Hexcolor("#F48348")]
-                          ),
-                          borderRadius: BorderRadius.circular(30)
+                ),
+                SizedBox(height: 10,),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Container(
+                      alignment: Alignment.center,
+                      width: 296,
+                      height: 52,                     
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Hexcolor("#EF5A53"), Hexcolor("#F48348"), Hexcolor("#F48348")]
                         ),
-                        child: Text("Sign In", style: TextStyle(color: Colors.white, fontFamily: "Quicksand-Medium"),),
+                        borderRadius: BorderRadius.circular(30)
                       ),
-                  ),
-                  SizedBox(height: 10,),
-                  Center(child: Text("Forgot Password?"),),
-                  SizedBox(height: 50,),
-                  Center(child: Text("Create a new account"),),
-                ],
-              ),
+                      child: Text("Login", style: TextStyle(fontSize: 16, color: Colors.white, fontFamily: "Quicksand-Medium"),),
+                    ),
+                ),
+                SizedBox(height: 10,),
+                Center(child: Text("Forgot Password?", style: TextStyle(fontFamily: "Quicksand-Medium"),),),
+                SizedBox(height: 50,),
+                Center(child: Text("Create a new account", style: TextStyle(fontSize: 16, fontFamily: "Quicksand-Bold"),),),
+              ],
             ),
           ),
         ),
